d3931522411ddcc2c7956a26c70a09684399df26
Ruben Montes <rubenmontes330@gmail.com>
2023-01-21 21:51:35 +0000

    Productos con estilo

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,13 +1,27 @@
 import { Component } from "react";
+const style= {
+    producto :{
+        border: 'solid 1px #eeee',
+        boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px'
+    },
+    img:{
+        width:'100%'
 
+    }
+}
 class Producto extends Component {
 
     render() {
        
         const { producto } = this.props
         return (
-            <div>
-                <img alt={producto.name} src={producto.img}></img>
+            <div style={style.producto}>
+                <img  style={style.img}
+                      alt={producto.name} 
+                      src={producto.img}></img>
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>    
             </div>
